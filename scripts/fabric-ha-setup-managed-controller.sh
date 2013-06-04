91440e7c59110ebd338ec84087644e556b890333
Scott Cranton <scott@cranton.com>
2013-06-04 10:40:57 -0400

    Update for test machines

--- scripts/fabric-ha-setup-managed-controller.sh
+++ scripts/fabric-ha-setup-managed-controller.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JBOSS_FUSE_HOME=~/opt/fuse/jboss-fuse-6.0.0.redhat-024
+JBOSS_FUSE_HOME=/root/opt/jboss-a-mq-6.0.0.redhat-024
 export JBOSS_FUSE_HOME
 
 CONTAINER_USERNAME=admin
@@ -8,6 +8,7 @@ CONTAINER_PASSWORD=admin
 CONTAINER_SSH_HOST=localhost
 CONTAINER_SSH_PORT=8101
 ZOOKEEPER_PASSWORD=admin
+DATA_DIR=/srv/fuse
 
 warn() {
     echo "${PROGNAME}: $*"
@@ -32,6 +33,6 @@ fi
 
 $KARAF_CLIENT -u $CONTAINER_USERNAME -p $CONTAINER_PASSWORD -h $CONTAINER_PASSWORD -a $CONTAINER_SSH_PORT <<- end
     fabric:create --clean --zookeeper-password $ZOOKEEPER_PASSWORD -p fmc root
-    fabric:mq-create --group a-mq-east --networks a-mq-west --networks-username $CONTAINER_USERNAME --networks-password $CONTAINER_PASSWORD a-mq-east-profile
-    fabric:mq-create --group a-mq-west --networks a-mq-east --networks-username $CONTAINER_USERNAME --networks-password $CONTAINER_PASSWORD a-mq-west-profile
+    fabric:mq-create --data $DATA_DIR --group a-mq-east --networks a-mq-west --networks-username $CONTAINER_USERNAME --networks-password $CONTAINER_PASSWORD a-mq-east-broker
+    fabric:mq-create --data $DATA_DIR --group a-mq-west --networks a-mq-east --networks-username $CONTAINER_USERNAME --networks-password $CONTAINER_PASSWORD a-mq-west-broker
 end
--- scripts/fabric-ha-setup-managed-west.sh
+++ scripts/fabric-ha-setup-managed-west.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JBOSS_FUSE_HOME=~/opt/fuse/jboss-fuse-6.0.0.redhat-024
+JBOSS_FUSE_HOME=/root/opt/jboss-a-mq-6.0.0.redhat-024
 export JBOSS_FUSE_HOME
 
 CONTAINER_USERNAME=admin
@@ -8,7 +8,7 @@ CONTAINER_PASSWORD=admin
 CONTAINER_SSH_HOST=localhost
 CONTAINER_SSH_PORT=8101
 ZOOKEEPER_PASSWORD=admin
-ZOOKEEPER_URL=localhost:2181
+ZOOKEEPER_URL=host117.phx.salab.redhat.com:2181
 
 warn() {
     echo "${PROGNAME}: $*"
@@ -32,5 +32,5 @@ if [ "x$JBOSS_FUSE_HOME" != "x" ]; then
 fi
 
 $KARAF_CLIENT -u $CONTAINER_USERNAME -p $CONTAINER_PASSWORD -h $CONTAINER_PASSWORD -a $CONTAINER_SSH_PORT <<- end
-    fabric:join --zookeeper-password $ZOOKEEPER_PASSWORD -p a-mq-west-profile $ZOOKEEPER_URL A-MQ-West
+    fabric:join --zookeeper-password $ZOOKEEPER_PASSWORD -p a-mq-west-broker $ZOOKEEPER_URL A-MQ-West
 end
